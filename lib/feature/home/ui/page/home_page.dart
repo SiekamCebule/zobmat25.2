78c61bc76876f742f5d04c185138267e37d55463
Konrad <konradkrol110@gmail.com>
2025-02-03 20:15:35 +0100

    Remove the what are distributions button

--- lib/feature/home/ui/page/home_page.dart
+++ lib/feature/home/ui/page/home_page.dart
@@ -142,7 +142,7 @@ class _HomePageState extends State<HomePage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Tooltip(
+              /*Tooltip(
                 message: 'Jeszcze nad tym pracujemy...',
                 child: OutlinedButton(
                   style: OutlinedButton.styleFrom(
@@ -154,7 +154,7 @@ class _HomePageState extends State<HomePage> {
                   child: Text('Ale... czym w ogóle są te rozkłady?'),
                 ),
               ),
-              Gap(20),
+              Gap(20),*/
               Gap(350),
               FilledButton(
                 style: FilledButton.styleFrom(
@@ -163,8 +163,8 @@ class _HomePageState extends State<HomePage> {
                 ),
                 onPressed: () {
                   context.read<NavigationCubit>().goTo(
-                    NavigationEntry.distributionsCatalogue,
-                  );
+                        NavigationEntry.distributionsCatalogue,
+                      );
                 },
                 child: Text('Przejdź do katalogu'),
               ),
